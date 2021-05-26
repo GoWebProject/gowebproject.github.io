1eec36279a54db8a49f4dc18b9ead48a71870fb5
mikairyuu <miva200267@gmail.com>
2021-05-26 20:55:02 +1000

    добавил потерянные классы

---
+++ WebApplication/Pages/LoginPage.cshtml.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.AspNetCore.Components;
+
+
+namespace WebApplication.Pages
+{
+    public class LoginPageModel : ComponentBase
+    {
+        public void OnGet()
+        {
+            
+        }
+
+        public void OnSubmitListener(NavigationManager navigationManager)
+        {
+            //TODO
+        }
+    }
+}
---
+++ WebApplication/Pages/PlayerList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication.Pages
+{
+    public class PlayerListModel : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}

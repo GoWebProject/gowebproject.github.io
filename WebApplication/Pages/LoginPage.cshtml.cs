7df8e708a74c0d8b31eac3c2a3c2f8ad9d885f45
mikairyuu <miva200267@gmail.com>
2021-05-27 11:37:27 +1000

    добавил плагин и первые попытки установить соединение с базой

--- WebApplication/Pages/LoginPage.cshtml.cs
+++ WebApplication/Pages/LoginPage.cshtml.cs
@@ -1,5 +1,8 @@
 using System;
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using MySql.Data.MySqlClient;
+using Ubiety.Dns.Core;
 
 
 namespace WebApplication.Pages
@@ -8,12 +11,16 @@ namespace WebApplication.Pages
     {
         public void OnGet()
         {
-            
         }
 
-        public void OnSubmitListener(NavigationManager navigationManager)
+        public void OnSubmitListener(NavigationManager navigationManager, IJSRuntime jsRuntime)
         {
-            //TODO
+            var connectionString = "Server=127.0.0.1; Port=54855; Database=localdb; Uid=azure; Pwd=6#vWHD_$";
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            conn.Open();
+            jsRuntime.InvokeVoidAsync("alert", conn.State.ToString());
+            jsRuntime.InvokeVoidAsync("alert", Environment.GetEnvironmentVariable("MYSQLCONNSTR_localdb"));
+            conn.Close();
         }
     }
 }

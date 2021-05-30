f710fade3b358bda5deba9a33e940f17f233c121
Bengeta <inovozhilov08@gmail.com>
2021-05-30 18:51:55 +1000

    Профиль с изменением профиля надо сделать еще изменение пароля

--- WebApplication/Models/User.cs
+++ WebApplication/Models/User.cs
@@ -1,13 +1,15 @@
-namespace WebApplication.Models
+﻿namespace WebApplication.Models
 {
     public class User
     {
-        public User(string username, string email, string pwd, string fullName, string passwordSalt = null)
+        public User(string username, string email, string pwd, string fullName, string passwordSalt = null, int rfg_rating = 1500, string misc_rating = "")
         {
             Username = username;
             PasswordHash = pwd;
             Email = email;
             FullName = fullName;
+            RFG_rating = rfg_rating;
+            Misc_rating = misc_rating;
             this.PasswordSalt = passwordSalt;
         }
 
@@ -16,5 +18,7 @@ namespace WebApplication.Models
         public string Email { get; set; }
         public string FullName { get; set; }
         public string PasswordSalt { get; set; }
+        public int RFG_rating { get; set; }
+        public string Misc_rating { get; set; }
     }
 }

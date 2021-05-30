67e40b11ce3102ad0173a6d8bc884cf127567006
Bengeta <inovozhilov08@gmail.com>
2021-05-30 19:10:27 +1000

    прошлый фикс не сработал, время для нового

--- WebApplication/Models/User.cs
+++ WebApplication/Models/User.cs
@@ -2,7 +2,7 @@
 {
     public class User
     {
-        public User(string username, string email, string pwd, string fullName, string passwordSalt = null, int rfg_rating = 1500, string misc_rating = "")
+        public User(string username, string email, string pwd, string fullName, string passwordSalt = null, string rfg_rating = "1500", string misc_rating = "")
         {
             Username = username;
             PasswordHash = pwd;
@@ -18,7 +18,7 @@
         public string Email { get; set; }
         public string FullName { get; set; }
         public string PasswordSalt { get; set; }
-        public int RFG_rating { get; set; }
+        public string RFG_rating { get; set; }
         public string Misc_rating { get; set; }
     }
 }

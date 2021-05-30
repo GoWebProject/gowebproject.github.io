be5bba8652d1551037d53858142b86327371b4ba
Bengeta <inovozhilov08@gmail.com>
2021-05-30 15:22:17 +1000

    фиксы

--- WebApplication/Pages/Profile.cshtml.cs
+++ WebApplication/Pages/Profile.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using WebApplication.Models;
 
 namespace WebApplication.Pages
 {
@@ -14,6 +15,15 @@ namespace WebApplication.Pages
 
         public void OnGet()
         {
+        }
+
+        protected bool OnSubmitListener(string username, string fullname, string email, string _pwd)
+        {
+            if ((username.Length > 0) && (fullname.Length > 0) && (email.Length > 0) && (_pwd.Length > 0) && (email.Contains('@')))
+            {
+                return UserManager.ChangeUser(,new User(username, email, _pwd, fullname));
+            }
+            return false;
         }
     }
 }

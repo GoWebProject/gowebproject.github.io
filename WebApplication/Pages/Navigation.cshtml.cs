311447bac061eb63f0a3c7aa9af45de5735a3c03
Allis Overlord <lina.leskova5891@gmail.com>
2021-06-17 00:07:54 +1000

    New page "Navigation"

---
+++ WebApplication/Pages/Navigation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication.Pages
+{
+    public class Navigation : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
